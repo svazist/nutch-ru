3315b2f2249e74783841ec56ac28cc677a25f90b
Marko Bauhardt <mbauhardt@yahoo.com>
2009-08-26 15:54:53 +0200

    dont throw exception if instance already exists

--- src/plugin/admin-instance/src/java/org/apache/nutch/admin/instance/InstanceController.java
+++ src/plugin/admin-instance/src/java/org/apache/nutch/admin/instance/InstanceController.java
@@ -24,8 +24,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.nutch.admin.ConfigurationUtil;
 import org.apache.nutch.admin.NavigationSelector;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,7 +46,7 @@ public class InstanceController extends NavigationSelector {
   public String[] referenceDataNames(HttpSession session) {
     ServletContext servletContext = session.getServletContext();
     ConfigurationUtil configurationUtil = (ConfigurationUtil) servletContext
-        .getAttribute("configurationUtil");
+            .getAttribute("configurationUtil");
     String[] allNames = configurationUtil.getAllNames();
     String[] allInstancesNames = new String[allNames.length - 1];
     int counter = 0;
@@ -63,27 +61,16 @@ public class InstanceController extends NavigationSelector {
 
   @RequestMapping(method = RequestMethod.POST)
   public String createIstance(
-      @ModelAttribute("createInstance") CreateInstanceCommandObject commandObject,
-      BindingResult bindingResult, HttpSession httpSession) throws IOException {
+          @ModelAttribute("createInstance") CreateInstanceCommandObject commandObject,
+          HttpSession httpSession) throws IOException {
     ServletContext servletContext = httpSession.getServletContext();
     ConfigurationUtil configurationUtil = (ConfigurationUtil) servletContext
-        .getAttribute("configurationUtil");
+            .getAttribute("configurationUtil");
     String folderName = commandObject.getFolderName();
-
-    // TODO refactore in a validator
-    ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "folderName",
-        "empty");
-    if (!bindingResult.hasErrors()) {
-      if (configurationUtil.existsConfiguration(folderName)) {
-        bindingResult.rejectValue("folderName", "alreadyExists");
-      }
+    if (folderName != null && !"".equals(folderName.trim())
+            && !configurationUtil.existsConfiguration(folderName)) {
+      configurationUtil.createNewConfiguration(folderName);
     }
-
-    if (bindingResult.hasErrors()) {
-      return "instance";
-    }
-
-    configurationUtil.createNewConfiguration(folderName);
     return "redirect:index.html";
   }
 
