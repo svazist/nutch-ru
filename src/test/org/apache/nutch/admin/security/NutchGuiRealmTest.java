17f7812a326604fb79238143a66779927b81650b
Marko Bauhardt <mbauhardt@yahoo.com>
2009-09-18 16:05:55 +0200

    NUTCHGUI-1: add license header and fix test

--- src/test/org/apache/nutch/admin/security/NutchGuiRealmTest.java
+++ src/test/org/apache/nutch/admin/security/NutchGuiRealmTest.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.nutch.admin.security;
 
 import java.security.Principal;
@@ -68,11 +84,7 @@ public class NutchGuiRealmTest extends TestCase {
 
   public void testGetName() throws Exception {
     UserRealm realm = new NutchGuiRealm();
-    try {
-      realm.getName();
-      fail();
-    } catch (UnsupportedOperationException e) {
-    }
+    assertEquals(NutchGuiRealm.class.getSimpleName(), realm.getName());
   }
 
   public void testDisassociate() throws Exception {
